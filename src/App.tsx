325d81e0a3b77287287c32e64c1eaeb7c6b153b1
Felipe Duarte <feduarte.dev@gmail.com>
2023-07-06 16:13:57 -0300

    segundo requisito completo

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,26 @@ import { useState } from 'react';
 import Book from './components/Book';
 import BookList from './components/BookList';
 import data from './data.json';
-
-const bookIndexStart = 10;
-const bookIndexEnd = 15;
-const placeHolderList = data.slice(bookIndexStart, bookIndexEnd); // esse código deverá ser excluído após a implementação do requisito 2
+import { BookInfoType } from './types/index';
 
 function App() {
   const [index, setIndex] = useState(0);
+  const [wishList, setwishList] = useState<BookInfoType[]>([]);
 
   function handleNextClick() {
-    if (index + 1 < data.length) {
-      setIndex(index + 1);
-    } else {
-      setIndex(0);
-    }
+    return index + 1 < data.length ? setIndex(index + 1) : setIndex(0);
   }
+
+  function handleAddClick() {
+    setwishList([...wishList, data[index]]);
+  }
+
   return (
     <div className="app">
       <div className="book-selector">
         <Book bookInfo={ data[index] } showDetails />
         <div className="selector-buttons">
-          <button>Adicionar à lista de desejos</button>
+          <button onClick={ handleAddClick }>Adicionar à lista de desejos</button>
           <button>Adicionar à lista de leitura</button>
           <button>Adicionar à lista de lidos</button>
           <button onClick={ handleNextClick }>Próximo livro</button>
@@ -36,7 +35,7 @@ function App() {
         <button>Exibir lista de lidos</button>
       </div>
       <h1>Lista de ...</h1>
-      <BookList books={ placeHolderList } />
+      <BookList books={ wishList } />
     </div>
   );
 }
